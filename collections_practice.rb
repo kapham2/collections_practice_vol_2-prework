dd866599ec9fc23face4b61c7b7b4748fabafb5a
Kim Pham <pham.kim.a@gmail.com>
2018-08-26 04:16:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -88,7 +88,7 @@ def organize_schools(schools)
   
   schools.each do |school, loc_hash|
     loc_hash.each do |key, value|
-      organized_hash[value] = school
+      organized_hash[value] = [school]
     end
   end
   
